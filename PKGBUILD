66dcf96e30c2d02eb8e91d71c4780c99bd0b3ab3
dingjing <dingjing@live.cn>
2022-04-30 12:37:39 +0800

    暂存

--- PKGBUILD
+++ PKGBUILD
@@ -15,6 +15,8 @@ makedepends=('docbook-xsl' 'gobject-introspection' 'parted' 'gtk-doc')
 optdepends=('gptfdisk: GUID partition table support'
             'ntfs-3g: NTFS filesystem management support'
             'dosfstools: VFAT filesystem management support')
+provides=("$_pkgname=$pkgver")
+conflicts=("$_pkgname")
 backup=('etc/udisks2/udisks2.conf')
 source=("https://github.com/graceful-linux/udisks/archive/refs/tags/udisks-debug-2.9.4.tar.gz")
 sha512sums=('3836f6d2d6fe02eb9cccbb85d940bb5b4a07db1dafe7beba1f2ded110b04de03b1e0b79e11592c3a9b2f0e047ae4fc4284d895a953e1f4dd042e544634e505ca')
--- push.sh
+++ push.sh
@@ -2,6 +2,7 @@
 
 pkg_name='graceful-udisks'
 work_dir=$(dirname $(realpath -- $0))
+build=$(which makepkg)
 
 echo $pkg_name
 echo $work_dir
@@ -11,7 +12,16 @@ cd $work_dir
 
 cd $work_dir && git clone ssh://aur@aur.archlinux.org/$pkg_name.git
 
-cp $work_dir/
+[ $? -ne 0 ] && echo 'execute error!' && exit
+[ ! -d $work_dir/$pkg_name ] && echo "$pkg_name project not exists!" && exit 1
+
+cd $work_dir && cp LICENSE PKGBUILD README.md $work_dir/$pkg_name
+
+[ ! -e $build ] && echo 'makepkg command not found!' && exit 1
+cd $work_dir/$pkg_name && $build --printsrcinfo > .SRCINFO
+
+# build
+cd $work_dir/$pkg_name && $build
 
 
 
